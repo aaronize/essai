b1d3e94ff2b4e4de6589e87bcd980a23b53b8d7a
aaronize <chendai2004@hotmail.com>
2019-09-07 23:47:29 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
 set -e
-. ./print.sh
 
 deploy_type=$1
 
+function print_info_log() {
+    echo "[INFO] `date "+%Y-%m-%d %H:%M:%S"` --> $1"
+}
+
+function print_error_log() {
+    echo "[ERROR] `date "+%Y-%m-%d %H:%M:%S"` --> $1"
+}
+
 function development() {
-    print_info_log "development deploy"
+    print_info_log "development deploy begin..."
     docker run -d --rm --network=host --name essai-api -p 13030:13030 essai-api:latest # >/dev/null 2>&1
 }
 
 function production() {
-    print_info_log "production deploy"
+    print_info_log "production deploy begin..."
 }
 
 if [[ "$deploy_type" == "development" ]]; then
