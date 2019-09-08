8ebea0d7181e17dc34c837693da47cc0bc67e3a9
aaronize <chendai2004@hotmail.com>
2019-09-08 18:58:21 +0800

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,10 +6,11 @@ function print_log() {
     echo "[$1] `date "+%Y-%m-%d %H:%M:%S"` --> $2"
 }
 
-print_log "INFO" "build begins"
+print_log "INFO" "build begins..."
 rm -f essai-api
 
 print_log "INFO" "go build"
+pwd
 CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o essai-api ./main.go
 
 print_log "INFO" "docker build"
