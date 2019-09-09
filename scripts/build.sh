0676395f95587246265259a4b00b208d48d08792
aaronize <chendai2004@hotmail.com>
2019-09-09 09:46:12 +0800

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CGO_ENABLED=0
+
 function print_log() {
     echo "[$1] `date "+%Y-%m-%d %H:%M:%S"` --> $2"
 }
@@ -10,8 +12,9 @@ print_log "INFO" "build begins..."
 rm -f essai-api
 
 print_log "INFO" "go build"
-pwd
-CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o essai-api ./main.go
+#GO_BUILD -o essai-api ./main.go
+docker run --rm -v /data/jenkins_home/workspace/essai-api:/go/src/essai-api golang:1.12-alpine \
+go build -a -ldflags '-extldflags "-static"' -o essai-api ./main.go
 
 print_log "INFO" "docker build"
 docker build -t essai-api:latest .
